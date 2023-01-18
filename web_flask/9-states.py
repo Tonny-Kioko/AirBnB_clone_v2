81bea46a110c08256fb18079f62e1e2a819980f9
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-18 23:28:03 +0300

    States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application:
+"""
+
+from flask import Flask
+from models import storage
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states_list_route():
+    """
+    List states: display a HTML page: (inside the tag BODY)
+    Returns:
+        html: template that lists all states sort by name A->Z
+    """
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_by_id_route(id):
+    """
+    Get a state by id
+    Returns:
+        html: template that lists cities of state sort by name A->Z
+    """
+    state = None
+    for s in storage.all("State").values():
+        if s.id == id:
+            state = s
+            break
+    return render_template("9-states.html", state=state)
+
+
+@app.teardown_appcontext
+def close_db(exception=None):
+    """
+    After each request remove the current SQLAlchemy Session:
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
