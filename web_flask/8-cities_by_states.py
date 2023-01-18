f48d6e409ebb16669d86ec2ff779e3d5c8c306f6
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-01-18 23:24:54 +0300

    polishing and corrections

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application:
+"""
+
+from flask import Flask
+from models import storage
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states_route():
+    """
+    Cities by states: display a HTML page: (inside the tag BODY)
+    Returns:
+        html: template that lists all states sort by name A->Z
+    """
+    states = storage.all("State").values()
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def close_db(exception=None):
+    """
+    After each request remove the current SQLAlchemy Session:
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
